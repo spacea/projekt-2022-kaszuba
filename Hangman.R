dfa85cedaa0bd34c8bbe4a56c713b53d6fb1fe2d
Karo665 <102479211+Karo665@users.noreply.github.com>
2022-03-30 15:30:33 +0200

    Add files via upload

---
+++ Hangman.R
@@ -0,0 +1,69 @@
+#hangman
+
+gameHangman = function(x) {
+  
+  cat(x,"\n")
+  
+  wektor = replicate(nchar(x), FALSE)
+  
+  
+  for(i in 1 : 15) {
+    
+    while(TRUE){
+      l = readline(prompt="Wpisz literę: ")
+      
+      if(nchar(l) == 1)break;
+      if(nchar(l) > 1){
+        cat("pierwsza litera\n")
+        break;
+      }
+      cat("niepodałeś litery\n")
+    }
+    podana_litera = l[1]
+    index = 1
+    liczba_zgadnietych = 0
+    t = 0
+    for(sprawdzna_litera in strsplit(x,"")[[1]])
+    {
+     
+      if(sprawdzna_litera == podana_litera)
+      {
+        if(wektor[index] == TRUE)
+        {
+          t = 2
+        }else{
+          t = 1
+        }
+        wektor[index] = TRUE
+      }
+      
+      if(wektor[index] == TRUE){
+        cat(sprawdzna_litera," ")
+        liczba_zgadnietych = liczba_zgadnietych+1
+      }else{
+        cat("_ ")
+      }
+      index = index+1
+    }
+    cat('\n')
+    if(t == 0){
+      cat("Nietrafiłeś \n")
+    }else if(t == 1){
+      cat("Trafiłeś\n")
+    }else{
+      cat("Już istnieje\n")
+    }
+    if(liczba_zgadnietych == nchar(x))
+    {
+      
+      return("wygrałeś")
+    }
+    
+  }
+  
+  return("przegrałeś")
+}
+
+
+
+gameHangman("wisielec")
